8154f3ee3bbb21a6f25c861965a1fe4656230e3b
t1ooo <5966878+t1ooo@users.noreply.github.com>
2024-08-10 14:58:53 +0500

    fix centralreg

--- src/grabber/nsreg/spiders/nsreg_centralreg.py
+++ src/grabber/nsreg/spiders/nsreg_centralreg.py
@@ -6,8 +6,8 @@ from ..base_site_spider import BaseSpiderComponent
 class NsregCentralregSpider(scrapy.Spider):
 
     name = "nsreg_centralreg_ru"
-    start_urls = ["https://www.centralreg.ru/tarify-i-oplata/"]
-    allowed_domains = ("www.centralreg.ru")
+    start_urls = ["https://centralreg.ru/tarify-i-oplata/"]
+    allowed_domains = ("centralreg.ru",)
     site_names = ("ООО «Перспектива»",)
 
     def __init__(self, *args, **kwargs):
@@ -18,9 +18,9 @@ class NsregCentralregSpider(scrapy.Spider):
             site_names=self.site_names,
             regex=r"\s{3}([0-9]{2,3})",
             path={
-                'price_reg': '/html/body/div[1]/div/div/div/div/div/div/div/div/div/div/article/div/div/div/div/div/table/tbody/tr[4]/td[2]/p/strong/text()',
-                'price_prolong': '/html/body/div[1]/div/div/div/div/div/div/div/div/div/div/article/div/div/div/div/div/table/tbody/tr[5]/td[2]/p/strong/text()',
-                'price_change': '/html/body/div[1]/div/div/div/div/div/div/div/div/div/div/article/div/div/div/div/div/table/tbody/tr[8]/td[2]/p/strong/text()'
+                'price_reg': '/html/body/div[3]/div[2]/div/div/article/div/div/div[2]/div/div[2]/div/div/div/div/table/tbody/tr[3]/td[2]/div/div/div/text()',
+                'price_prolong': '/html/body/div[3]/div[2]/div/div/article/div/div/div[2]/div/div[2]/div/div/div/div/table/tbody/tr[4]/td[2]/div/div/div/text()',
+                'price_change': '/html/body/div[3]/div[2]/div/div/article/div/div/div[2]/div/div[2]/div/div/div/div/table/tbody/tr[7]/td[2]/div/div/div/text()'
                 }
             )
 
